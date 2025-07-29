5cb2a88aaec1c392abc9514c4f76aa8694c5112a
Ashenafi Tsegaye <ashenafitsegish@gmail.com>
2025-07-29 22:18:55 +0300

    fixed sidebar bug

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -46,11 +46,6 @@ export const Sidebar = ({sideTog}) => {
                             <BsBriefcase size={20} />View All Jobs
                         </Link>
                     </div>
-{/*                     <div className='flex justify-center gap-2 items-center'>
-                        <Link  to="/admin/allApplications" className='flex blueCol px-4 py-1 justify-center gap-2 items-center'>
-                            <MdOutlineFeaturedPlayList size={20} />
-
-                            View All Applications </Link></div> */}
 
                     <div className='flex justify-center gap-2 items-center'>
                         <Link to="/admin/allUsers" className='flex blueCol px-4 py-1 justify-center gap-2 items-center'>
