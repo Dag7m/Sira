0714b1c0e2736bec781b2d257d1d137e172d2ce9
Ashenafi Tsegaye <ashenafitsegish@gmail.com>
2025-10-02 10:55:07 +0300

    added feature

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -307,11 +307,6 @@ export const MyProfile = () => {
                           Delete Account
                         </button>
                       </Link>
-                    {/*   <Link to="/editProfile">
-                        <button className="bg-blue-600/80 hover:bg-blue-500/90 w-full text-white font-semibold px-6 py-2 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 transform hover:scale-105">
-                          Edit Profile
-                        </button>
-                      </Link> */}
                     </div>
                   </div>
                 </div>
